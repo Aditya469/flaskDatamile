b868a430e18af55a9f104ddebe7f06417936720d
Rama <rama@ecam.co.uk>
2024-07-17 14:14:54 +0100

    Added server configs

---
+++ flask_service.py
@@ -0,0 +1,39 @@
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+import socket
+import sys
+import os
+from waitress import serve
+from run import app
+
+class FlaskService(win32serviceutil.ServiceFramework):
+    _svc_name_ = "FlaskService"
+    _svc_display_name_ = "Flask Application Service"
+
+    def __init__(self, args):
+        win32serviceutil.ServiceFramework.__init__(self, args)
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+        socket.setdefaulttimeout(60)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+
+    def SvcDoRun(self):
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
+                              servicemanager.PYS_SERVICE_STARTED,
+                              (self._svc_name_, ''))
+        self.main()
+
+    def main(self):
+        serve(app, host='0.0.0.0', port=90)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        servicemanager.Initialize()
+        servicemanager.PrepareToHostSingle(FlaskService)
+        servicemanager.StartServiceCtrlDispatcher()
+    else:
+        win32serviceutil.HandleCommandLine(FlaskService)
--- run_flask_app.bat
+++ run_flask_app.bat
@@ -1,10 +1,7 @@
 @echo off
-echo Starting Nginx... > C:\Users\Rama\flaskDatamile\service.log
 cd C:\nginx
-start nginx >> C:\Users\Rama\flaskDatamile\service.log 2>&1
+start nginx
 
-timeout /t 10 /nobreak
-
-echo Starting Flask application... >> C:\Users\Rama\flaskDatamile\service.log
 cd C:\Users\Rama\flaskDatamile
-start /B venv\Scripts\activate && python wsgi.py >> C:\Users\Rama\flaskDatamile\service.log 2>&1
+start /B venv\Scripts\activate && python wsgi.py
+
